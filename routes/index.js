292bffbf8672993200aff826ece87b14175709b8
Brian Leonard <brian.leonard@mongodb.com>
2019-02-10 10:44:04 -0500

    Added results

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Atlas Data Importer" });
 });
 
-router.post('/', function(req, res) {
+router.post("/", function(req, res) {
   console.log(req.body.host);
   console.log(req.body.user);
 
-  var exec = require('child_process').exec;
-  var command = 'mongoimport --host ' + req.body.host + ' --ssl --username ' + req.body.user + ' --password ' + req.body.password + ' --authenticationDatabase admin --db Workshop --collection Restaurants --type JSON --file primer-dataset.json'
-  exec(command, function (error, stdout, stderr) {
-	if (!error) {
-		console.log ("Data imported successfully!");
-	} else
-		console.log(error);
-	}
-   );
+  var exec = require("child_process").exec;
+  var command =
+    "mongoimport --host " +
+    req.body.host +
+    " --ssl --username " +
+    req.body.user +
+    " --password " +
+    req.body.password +
+    " --authenticationDatabase admin --db Workshop --collection Restaurants --type JSON --file primer-dataset.json";
+  exec(command, function(error, stdout, stderr) {
+    if (!error) {
+      console.log(stdout)
+      var result = "Data imported successfully";
+      console.log(result);
+      res.render("index", { result: result });
+    } else {
+      console.log(error);
+      res.render("index", { result: error });
+    }
+  });
 
 });
-
 
 module.exports = router;
